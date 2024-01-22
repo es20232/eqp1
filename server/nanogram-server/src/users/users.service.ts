5c8513fc34a5287629e6857123182fc7974d5343
Italo borges <Italoborgessantos40@gmail.com>
2024-01-22 20:58:53 -0300

    API documentation working

--- server/nanogram-server/src/users/users.service.ts
+++ server/nanogram-server/src/users/users.service.ts
@@ -6,46 +6,67 @@ import * as argon from 'argon2';
 @Injectable()
 export class UsersService {
     constructor(private prisma: PrismaService){}
-    async get(){
-        return 'me';
-    }
-    async getUser(username:string){
+    async get(id: number){
+      try{
         const user= await this.prisma.user.findUnique({
-          where:{
-              username,
-          },
-        });
-        if(!user){
-            throw new NotFoundException("Usuário não encontrado");
-        }
-        return user;
+        where:{
+            id,
+        },
+
+      });
+      delete user.password;
+      return user;
     }
-    async update(dto: usersupdateDto,username: string,image?: profile_picDto,){
-      this.getUser(username);
+      catch(error){
+        console.error(`Error : ${error.message}`);
+        throw new NotFoundException('User not found.');
+      }
       
+    }
+   
+    async update(dto: usersupdateDto,id: number,image?: profile_picDto,){
+      this.get(id);
+     
       if(dto.password){
            dto.password = await argon.hash(dto.password);
       }  
       if(!image){ 
-        return await this.prisma.user.update({
+        const user= await this.prisma.user.update({
           where: {
-              username,
+              id,
             },
             data: {
               ...dto,
              
             },
           });
+          delete user.password
+          return user;
         }
-        return await this.prisma.user.update({
+        const user= await this.prisma.user.update({
           where: {
-              username,
+              id,
             },
             data: {
               ...dto,
               profile_picture:image.buffer,
             },
           });
+          delete user.password;
+          return user;
     }
+    async delete(id: number){
+       this.get(id);
+       const user= await this.prisma.user.delete(
+        {
+          where:{
+            id,
+          }
+        }
+       )
+       delete user.password;
+       return user;
+    }
+   
    
 }
