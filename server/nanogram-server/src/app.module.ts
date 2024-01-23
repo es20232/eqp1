1be2e7ca19ff4c9b810ad4f2df403fee05154d79
landerson98n <landerson98n@gmail.com>
2024-01-22 23:28:03 -0300

    t

--- server/nanogram-server/src/app.module.ts
+++ server/nanogram-server/src/app.module.ts
@@ -4,13 +4,11 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersService } from './users/users.service';
-<<<<<<< HEAD
-=======
 import { MailModule } from './mail/mail.module';
->>>>>>> 5c8513fc34a5287629e6857123182fc7974d5343
+
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule],//, MailModule],
+  imports: [AuthModule, UsersModule, PrismaModule, MailModule],
   controllers: [UsersController],
   providers: [UsersService],
   
