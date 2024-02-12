57d55f9175d18d0b5f6b9832bef69e7e59e49959
italob <italoborgessantos40@gmail.com>
2024-02-12 00:00:25 -0300

    get and update  AXIOS Requests

---
+++ client/src/actions/user.ts
@@ -0,0 +1,70 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import axios from 'axios';
+
+export interface updtUser {
+    full_name?: string;
+    username?: string;
+    email?: string;
+    password?: string;
+  }
+  export interface picture{
+    profile_picture?: File;
+  }
+  export async function edit_profile(user:updtUser,image: picture){
+    const token=cookies().get('token_user');
+    const response=await axios.patch
+    ('http://localhost:3001/users/edit-profile'
+    ,{user,image},
+    {
+        
+        headers: {
+            "Authorization":"Bearer "+ token,
+            'Content-type': 'multipart/form-data charset=UTF-8'} 
+        
+    }
+    ).then(response=>{
+      return response.data
+    }).catch(error =>{
+        if (error.response) {
+            if (error.response.status == 400) {
+                throw error.response.data.message;
+            } else {
+                throw 'Unexpected error';
+            }
+        }
+
+    })
+
+    return redirect('/Profile');
+    }
+
+    export async function get_me(){
+        const token=cookies().get('token_user');
+        const response=await axios.get
+        ('http://localhost:3001/users/profile',
+        {
+            
+            headers: {
+                "Authorization":"Bearer "+ token,
+                'Content-type': 'application/json; charset=UTF-8'} 
+            
+        }
+        ).then(response=>{
+          return response.data
+        }).catch(error =>{
+            if (error.response) {
+                if (error.response.status == 404) {
+                    throw error.response.data.message;
+                } else {
+                    throw error;
+                }
+            }
+    
+        })
+        return response.data;
+    
+        }     
+        
