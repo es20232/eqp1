769caa40f79082ceb5ad6bba602a6c8af9678c0b
italob <italoborgessantos40@gmail.com>
2024-02-18 13:47:14 -0300

    complete Prisma_Model

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,11 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersService } from './users/users.service';
 import { MailModule } from './mail/mail.module';
+import { InteractionsModule } from './interactions/interactions.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, MailModule],
+  imports: [AuthModule, UsersModule, PrismaModule, MailModule, PostsModule, InteractionsModule],
   controllers: [UsersController],
   providers: [UsersService],
   
